7a1a036058717ff36ed21437eaae8fe0a867f9af
Paulo Cunha <gotencunha@gmail.com>
2024-01-05 14:38:27 +0000

    log file checks in place, sync time checks

--- arguments.py
+++ arguments.py
@@ -7,6 +7,8 @@ def check_arguments() -> dict:
         return None
     if not check_folder_paths():
         return None
+    if not check_sync_time():
+        return None
     if not check_log_file_path():
         return None
     return arguments
@@ -23,13 +25,30 @@ def check_number_args() -> bool:
     return True
 
 def check_folder_paths() -> bool:
-    if not os.path.exists(sys.argv[1]):
+    if not os.path.isdir(sys.argv[1]):
         print("Error: Invalid source path")
         return False
-    if not os.path.exists(sys.argv[2]):
+    elif not os.path.isdir(sys.argv[2]):
         print("Error: Invalid replica path")
         return False
     return True
 
 def check_log_file_path() -> bool:
+    folder = os.path.split(sys.argv[4])[0]
+    file = os.path.split(sys.argv[4])[1]
+
+    if not os.path.isdir(folder):
+        print("Error: Log file directory not found")
+        return False
+    elif not file.endswith(".csv") or len(file.split(".csv")[0]) < 1:
+        print("Error: Please specify a example.csv file")
+        return False
+    if not os.path.isfile(sys.argv[4]):
+        print("Log: File not found, a new one will be created")
+    return True
+
+def check_sync_time() -> bool:
+    if not sys.argv[3].isnumeric() or not int(sys.argv[3]) > 0:
+        print("Error: Sync time isnt an integer > 0")
+        return False
     return True
