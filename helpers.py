75c244a10430dcf38d6277ea93d9c4008f7e8952
Paulo Cunha <gotencunha@gmail.com>
2024-01-07 19:03:15 +0000

    Added detailed warnings for python version

--- helpers.py
+++ helpers.py
@@ -5,3 +5,5 @@ def check_python_version():
         or (sys.version_info[0] == 3 and sys.version_info[1] < 12):
             print("Log: Python version older then development version")
             print("Log: Application developed on Python 3.12.1")
+            print("Log: hashlib.file_digest was only added on 3.11")
+            print("Log: type hinting was added on 3.5")
