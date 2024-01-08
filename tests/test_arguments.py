6b8555ae8634dc84c1ac58a01c15e39ea925882c
Paulo Cunha <gotencunha@gmail.com>
2024-01-08 15:59:01 +0000

    Initial arguments unit testing

---
+++ tests/test_arguments.py
@@ -0,0 +1,93 @@
+import sys, os
+from unittest.mock import patch
+
+from arguments \
+import \
+check_arguments, \
+check_arg_count, \
+check_folder_paths, \
+check_log_file_path, \
+check_sync_time, \
+args_to_dict
+
+if (os.path.sep == "\\"):
+    log = "log\\log.csv"
+    log_txt = "log\\log.txt"
+elif (os.path.sep == "/"):
+    log = "log/log.csv"
+    log_txt = "log/log.txt"
+
+def test_check_arguments():
+    test_args = ["main.py", "source", "replica", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arguments() == { \
+                                        "source" : os.path.abspath(test_args[1]), \
+                                        "replica" : os.path.abspath(test_args[2]), \
+                                        "sync" : test_args[3], \
+                                        "log_dir" : os.path.abspath(os.path.split(sys.argv[4])[0]), \
+                                        "log_file" : os.path.split(sys.argv[4])[1]
+                                    }
+    test_args = ["main.py", "source", "replica", "2" , log_txt]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arguments() == {}
+    test_args = ["main.py", "source", "replica", "a" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arguments() == {}
+    test_args = ["main.py", "source", "", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arguments() == {}
+    test_args = ["main.py", " ", "replica", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arguments() == {}
+    test_args = ["main.py", "source", "replica", "1" , " "]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arguments() == {}
+    test_args = ["main.py", "source", "replica", "1" , "a"]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arguments() == {}
+
+def test_check_arg_count():
+    test_args = ["main.py", "source", "replica", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arg_count() == True
+    test_args = ["main.py", "", "replica", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arg_count() == False
+    test_args = ["main.py", "source", " ", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arg_count() == False
+    test_args = ["main.py"]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arg_count() == False
+    test_args = ["main.py", "source", "replica", "1" , log, "testing"]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arg_count() == False
+    test_args = ["main.py", "source", "replica", "1" ]
+    with patch.object(sys, 'argv', test_args):
+        assert check_arg_count() == False
+
+def test_check_folder_paths():
+    test_args = ["main.py", "source", "replica", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_folder_paths() == True
+    test_args = ["main.py", "source", "source", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_folder_paths() == False
+    test_args = ["main.py", "source", "source" + os.path.sep + "replica" , "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_folder_paths() == False
+    test_args = ["main.py", " ", "replica", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_folder_paths() == False
+    test_args = ["main.py", "source", "", "1" , log]
+    with patch.object(sys, 'argv', test_args):
+        assert check_folder_paths() == False
+
+def test_check_log_file_path():
+    pass
+
+def test_check_sync_time():
+    pass
+
+def test_args_to_dict():
+    pass
